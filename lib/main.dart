1f9b608817a7352528c808a34d7a9339241dfbbd
Leo Wojahn <leowojahn@gmail.com>
2025-04-26 13:42:57 -0300

    VsFinal

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,98 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MeuApp(),
+  );
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/profile-image.jpeg'),
+                radius: 50,
+              ),
+              Text(
+                'Leonardo Zitzke Wojahn',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'ESTUDANTE FLUTTER',
+                style: TextStyle(
+                  color: Colors.deepOrange.shade100,
+                  fontFamily: 'Source Sans',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 250,
+                child: Divider(
+                  color: Colors.deepOrange.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                  ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 25,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    '+55 (51) 99699-6508',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontFamily: 'Source Sans',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                  ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    'leowojahn@gmail.com',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontFamily: 'Source Sans',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
